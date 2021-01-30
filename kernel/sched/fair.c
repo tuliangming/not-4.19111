6f79bde14b7555e51b50d2276fe45b8caf95e906
Diab Neiroukh <lazerl0rd@thezest.dev>
2021-01-30 22:45:24 +0000

    sched/fair: Skip searching for active CPU when a tasks is marked as prefer_high_cap.
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7191,6 +7191,12 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 					continue;
 
 				/*
+				 * Skip searching for active CPU for tasks have
+				 * prefer_high_cap.
+				 */
+				if (schedtune_prefer_high_cap(p))
+					continue;
+				/*
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
