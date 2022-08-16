46f6eafc70ab67f3cac51bf93632a20a95d981cc
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-16 11:45:28 +0530

    mm: vmscan: Increase swappiness to 160
    Change-Id: Ia8214665ac43fc86be589ad29201db5b2a61f630
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -198,7 +198,7 @@ static u64 workingset_protection_prev_totalram __read_mostly = 0;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 160;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
