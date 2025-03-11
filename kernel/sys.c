8a76692965d6ca8e2e189d217056068fee7d8703
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    syscall: Only spoof uname for root

--- kernel/sys.c
+++ kernel/sys.c
@@ -1253,9 +1253,11 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "6.6.40");
-		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+	       if (current_uid().val == 0) {
+		       strcpy(tmp.release, "6.6.40");
+		       pr_debug("fake uname: %s/%d release=%s\n",
+			        current->comm, current->pid, tmp.release);
+	       }
 	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
