800922d0eb23b36285d8c1c06859434241c33a7b
skye-pa1n <sonicgames772@gmail.com>
2025-11-08 09:07:01 -0300

    drivers: kernelsu: checkout to skye-tachyon/KrenlolSU-Next@1115e94

--- drivers/kernelsu/file_wrapper.h
+++ drivers/kernelsu/file_wrapper.h
@@ -1,3 +1,6 @@
+#ifndef __KSU_H_FILE_WRAPPER
+#define __KSU_H_FILE_WRAPPER
+
 #include <linux/file.h>
 #include <linux/fs.h>
 
@@ -6,5 +9,7 @@ struct ksu_file_wrapper {
 	struct file_operations ops;
 };
 
-struct ksu_file_wrapper *mksu_create_file_wrapper(struct file *fp);
-void mksu_delete_file_wrapper(struct ksu_file_wrapper *data);
+struct ksu_file_wrapper *ksu_create_file_wrapper(struct file *fp);
+void ksu_delete_file_wrapper(struct ksu_file_wrapper *data);
+
+#endif
--- drivers/kernelsu/supercalls.c
+++ drivers/kernelsu/supercalls.c
@@ -335,6 +335,7 @@ static int do_get_wrapper_fd(void __user *arg)
 		return -1;
 	}
 
+	const char *anon_name = "[ksu_fdwrapper]";
 	struct ksu_get_wrapper_fd_cmd cmd;
 	int ret;
 
@@ -348,21 +349,30 @@ static int do_get_wrapper_fd(void __user *arg)
 		return -EBADF;
 	}
 
-	struct ksu_file_wrapper *data = mksu_create_file_wrapper(f);
+	struct ksu_file_wrapper *data = ksu_create_file_wrapper(f);
 	if (data == NULL) {
 		ret = -ENOMEM;
 		goto put_orig_file;
 	}
 
-	struct file *pf = anon_inode_getfile("[mksu_fdwrapper]", &data->ops,
-					     data, f->f_flags);
-	if (IS_ERR(pf)) {
-		ret = PTR_ERR(pf);
-		pr_err("mksu_fdwrapper: anon_inode_getfile failed: %ld\n",
-		       PTR_ERR(pf));
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0)
+#define getfd_secure anon_inode_create_getfd
+#elif defined(KSU_HAS_GETFD_SECURE)
+#define getfd_secure anon_inode_getfd_secure
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0) || defined(KSU_HAS_GETFD_SECURE)
+	ret = getfd_secure(anon_name, &data->ops, data, f->f_flags, NULL);
+#else
+	ret = anon_inode_getfd(anon_name, &data->ops, data, f->f_flags);
+#endif
+
+	if (ret < 0) {
+		pr_err("ksu_fdwrapper: getfd failed: %d\n", ret);
 		goto put_wrapper_data;
 	}
 
+	struct file *pf = fget(ret);
 	struct inode *wrapper_inode = file_inode(pf);
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0) ||                           \
 	defined(KSU_OPTIONAL_SELINUX_INODE)
@@ -375,21 +385,11 @@ static int do_get_wrapper_fd(void __user *arg)
 		sec->sid = ksu_file_sid;
 	}
 
-	ret = get_unused_fd_flags(cmd.flags);
-	if (ret < 0) {
-		pr_err("mksu_fdwrapper: get unused fd failed: %d\n", ret);
-		goto put_wrapper_file;
-	}
-
-	// pr_info("mksu_fdwrapper: installed wrapper fd for %p %d (flags=%d, mode=%d) to %p %d (flags=%d, mode=%d)", f, cmd.fd, f->f_flags, f->f_mode, pf, ret, pf->f_flags, pf->f_mode);
-	// pf->f_mode |= FMODE_READ | FMODE_CAN_READ | FMODE_WRITE | FMODE_CAN_WRITE;
-	fd_install(ret, pf);
+	fput(pf);
 	goto put_orig_file;
 
-put_wrapper_file:
-	fput(pf);
 put_wrapper_data:
-	mksu_delete_file_wrapper(data);
+	ksu_delete_file_wrapper(data);
 put_orig_file:
 	fput(f);
 
