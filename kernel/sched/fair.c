50137002880d06ce55d1e212054dd3c4a8a919d8
skye-pa1n <sonicgames772@gmail.com>
2025-11-08 14:11:25 -0300

    sched/fair: Make task_hot always return 0

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8933,31 +8933,7 @@ struct lb_env {
  */
 static int task_hot(struct task_struct *p, struct lb_env *env)
 {
-	s64 delta;
-
-	lockdep_assert_held(&env->src_rq->lock);
-
-	if (p->sched_class != &fair_sched_class)
-		return 0;
-
-	if (unlikely(task_has_idle_policy(p)))
-		return 0;
-
-	/*
-	 * Buddy candidates are cache hot:
-	 */
-	if (sched_feat(CACHE_HOT_BUDDY) && env->dst_rq->nr_running &&
-	    (&p->se == cfs_rq_of(&p->se)->next))
-		return 1;
-
-	if (sysctl_sched_migration_cost == -1)
-		return 1;
-	if (sysctl_sched_migration_cost == 0)
-		return 0;
-
-	delta = rq_clock_task(env->src_rq) - p->se.exec_start;
-
-	return delta < (s64)sysctl_sched_migration_cost;
+	return 0;
 }
 
 #ifdef CONFIG_NUMA_BALANCING
