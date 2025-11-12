303649a3b3f161163a924500492e933715320a6f
skye-pa1n <sonicgames772@gmail.com>
2025-11-12 12:11:38 -0300

    drivers: cpufreq: qcom: Clean up samsung changes

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -17,13 +17,6 @@
 #include <linux/sched.h>
 #include <linux/cpu_cooling.h>
 
-#include <linux/sec_debug.h>
-#include <linux/sec_smem.h>
-#include <trace/events/power.h>
-
-#define CREATE_TRACE_POINTS
-#include <trace/events/dcvsh.h>
-
 #define LUT_MAX_ENTRIES			40U
 #define CORE_COUNT_VAL(val)		(((val) & (GENMASK(18, 16))) >> 16)
 #define LUT_ROW_SIZE			32
@@ -34,11 +27,6 @@
 
 #define CYCLE_CNTR_OFFSET(c, m, acc_count)				\
 			(acc_count ? ((c - cpumask_first(m) + 1) * 4) : 0)
-
-
-#ifdef CONFIG_SEC_PM
-extern void *thermal_ipc_log;
-#endif
 
 enum {
 	CPUFREQ_HW_LOW_TEMP_LEVEL,
@@ -93,10 +81,6 @@ struct cpufreq_qcom {
 	char dcvsh_irq_name[MAX_FN_SIZE];
 	bool is_irq_enabled;
 	bool is_irq_requested;
-#ifdef CONFIG_SEC_PM
-	unsigned long lowest_freq;
-	bool limiting;
-#endif
 };
 
 struct cpufreq_counter {
@@ -174,17 +158,7 @@ static unsigned long limits_mitigation_notify(struct cpufreq_qcom *c,
 
 	arch_set_thermal_pressure(&c->related_cpus, min_t(unsigned long, 0,
 				  max_capacity - capacity));
-	trace_dcvsh_freq(cpumask_first(&c->related_cpus), freq);
 	c->dcvsh_freq_limit = freq;
-
-#ifdef CONFIG_SEC_PM
-	if (c->limiting == false) {
-		THERMAL_IPC_LOG("Start lmh cpu%d @%lu\n",
-			cpumask_first(&c->related_cpus), freq);
-		c->lowest_freq = freq;
-		c->limiting = true;
-	}
-#endif
 
 	return freq;
 }
@@ -205,10 +179,6 @@ static void limits_dcvsh_poll(struct work_struct *work)
 	dcvsh_freq = qcom_cpufreq_hw_get(cpu);
 
 	if (freq_limit != dcvsh_freq) {
-#ifdef CONFIG_SEC_PM
-	if ((c->limiting == true) && (freq_limit < c->lowest_freq))
-			c->lowest_freq = freq_limit;
-#endif
 		mod_delayed_work(system_highpri_wq, &c->freq_poll_work,
 				msecs_to_jiffies(LIMITS_POLLING_DELAY_MS));
 	} else {
@@ -221,13 +191,6 @@ static void limits_dcvsh_poll(struct work_struct *work)
 
 		c->is_irq_enabled = true;
 		enable_irq(c->dcvsh_irq);
-#ifdef CONFIG_SEC_PM
-		THERMAL_IPC_LOG("Fin. lmh cpu%d, "
-			"lowest %lu, f_lim %lu, dcvsh %lu\n",
-			cpu, c->lowest_freq, freq_limit, dcvsh_freq);
-		c->limiting = false;
-		c->lowest_freq = UINT_MAX;
-#endif
 	}
 
 	mutex_unlock(&c->dcvsh_lock);
@@ -339,12 +302,6 @@ qcom_cpufreq_hw_target_index(struct cpufreq_policy *policy,
 	} else {
 		writel_relaxed(index, c->reg_bases[REG_PERF_STATE]);
 	}
-
-	sec_smem_clk_osm_add_log_cpufreq(policy->cpu,
-				policy->freq_table[target_index].frequency, policy->kobj.name);
-
-	sec_smem_clk_osm_add_log_cpufreq(policy->cpu,
-				policy->freq_table[index].frequency, policy->kobj.name);
 
 	arch_set_freq_scale(policy->related_cpus,
 			    policy->freq_table[index].frequency,
